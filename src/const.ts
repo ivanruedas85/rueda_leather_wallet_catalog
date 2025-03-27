36ac0e3cece6286f0c405c7ece99c05f9779b302
ivanruedas85 <ivanruedas85@gmail.com>
2025-03-27 16:34:42 -0600

    Se agregaron nombre a usar

---
+++ src/const.ts
@@ -0,0 +1,5 @@
+export const NAME_SITE = "Rueda Leather Wallets"
+export const NAME_SITE_MIN = "RLW"
+export const AUTHOR_NAME_SITE = "ivanruedas85"
+export const AUTHOR_MAIL = "darthmaul666@protonmail.com"
+export const AUTHOR_NUMBER_PHONE = "+529371389398"
