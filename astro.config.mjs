c7e15e516fdddb1b2bac6856a813d962b66cfe73
ivanruedas85 <ivanruedas85@gmail.com>
2025-03-28 14:56:54 -0600

    Se agrego el repo en base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ruedaleatherwallet.github.io'
+    site: 'https://ruedaleatherwallet.github.io',
+    base: "rueda_leather_wallet_catalog"
 });
