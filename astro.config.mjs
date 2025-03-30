c2577f17573669d395a329ed19377307dcb0795f
ivanruedas85 <ivanruedas85@gmail.com>
2025-03-30 11:10:15 -0600

    Se eliminó base: y se cambió la url de site:

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ruedaleatherwallet.github.io',
-    base: "rueda_leather_wallet_catalog"
+    site: 'https://example.com',
+    //base: "rueda_leather_wallet_catalog"
 });
