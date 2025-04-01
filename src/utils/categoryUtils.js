3b9741d253f983da5d58a72d6cc8028cf973e876
ivanruedas85 <ivanruedas85@gmail.com>
2025-04-01 10:19:57 -0600

    Se creo y refactorizo

---
+++ src/utils/categoryUtils.js
@@ -0,0 +1,12 @@
+// filepath: c:\Users\Ivan Rueda\Documents\Github\Astro\rueda_leather_wallet_catalog\src\utils\categoryUtils.js
+import Products from '../data/Products.json';
+
+export function getCategoryData() {
+    const { products } = Products;
+    const categories = [...new Set(products.map((product) => product.category))];
+
+    return categories.map((category) => ({
+        params: { category: category.toLowerCase().replace(/\s+/g, '-') },
+        props: { category, products: products.filter((p) => p.category === category) },
+    }));
+}
