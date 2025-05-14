3537e4ac11c6353384118ac969aa855e10499388
ivanruedas85 <ivanruedas85@gmail.com>
2025-05-14 02:59:22 -0600

    Se ajusto la fecha para que el banner del dia del padre aparezca

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,7 +1,7 @@
-export const SPECIAL_START_DATE = new Date('2025-05-13')
-export const SPECIAL_END_DATE = new Date('2025-05-15')
+export const SPECIAL_START_DATE = new Date('2025-05-25')
+export const SPECIAL_END_DATE = new Date('2025-06-15')
 
-export function isSpecialActive(): boolean{
+export function isSpecialActive(): boolean {
     const now = new Date()
     return now >= SPECIAL_START_DATE && now <= SPECIAL_END_DATE
 }
