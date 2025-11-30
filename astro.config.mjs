f0dfc7490301e69568d8dbb4290f883074010291
ivanruedas85 <ivanruedas85@gmail.com>
2025-11-30 17:42:13 -0600

    update to uoput static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    //base: "rueda_leather_wallet_catalog"
+    //base: "rueda_leather_wallet_catalog",
+    output: "static"
 });
